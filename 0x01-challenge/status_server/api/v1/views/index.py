f61856d0898be0ce01adf22a886e8e7b3c1e970d
Hassan Cissey <hcissey0@gmail.com>
2023-10-13 03:55:01 +0000

    Fixed Flask #4

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -4,3 +4,10 @@
 from flask import jsonify
 
 from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of the web server
+    """
+    return jsonify({"status": "OK"})
