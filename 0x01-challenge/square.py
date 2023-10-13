84280cbc4b00fdc0b52e23eb25f1cc9facdfa24d
Hassan Cissey <hcissey0@gmail.com>
2023-10-13 04:36:24 +0000

    Fixed Square #2

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """This is the class module"""
 
 
-class square():
+class Square():
     """This is the square class"""
 
     width = 0
@@ -17,7 +17,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """The perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -28,7 +28,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
