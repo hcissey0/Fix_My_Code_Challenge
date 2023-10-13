bbcf94c78cd2164b323131322dbbb0f73020f278
Hassan Cissey <hcissey0@gmail.com>
2023-10-13 04:09:39 +0000

    Fixed Square #5

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""This is the square module"""
+"""This is the class module"""
 
 
 class square():
     """This is the square class"""
 
-    side = 0
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """This is the initializer"""
+        """THis is the constructor"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.side * self.side
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """The perimeter of the square"""
-        return (self.side * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """The string representation of the the squre"""
-        return "{}/{}".format(self.side, self.side)
+        """ The sting rep of the square"""
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(side=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
